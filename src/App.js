82a4fa4a7e7a367b60eb7d7921cc1b0b89a9c6e8
Galit Dotan <galitdotan99@gmail.com>
2023-03-28 19:31:41 +0300

    Step 3: basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Gala's cool app :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Stack spacing={3} sx={{marginTop: '20%'}} >
+                    <TextField id="bukcket-name-input" label="Bucket Name" variant="filled" color='primary'/>
+                    <Button sx={{maxWidth: "xs"}} variant="contained"> Scan Now </Button>
+                </Stack>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
